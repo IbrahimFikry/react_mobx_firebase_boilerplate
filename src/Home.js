a973520c7795065a8f2abd8d13ac12cc4f9492ab
Ibrahim <baim008@gmail.com>
2018-09-14 15:30:11 +0800

    added Log out function

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,27 @@
 import React from "react";
+import app from "./base";
 
-const Home = () => {
-  return (
-    <h1>Home</h1>
-  )
+class Home extends React.Component{
+
+  _logout = () => {
+    console.log();
+    app.auth().signOut().then(function() {
+      // Sign-out successful.
+      console.log("Sign out successful");
+    }).catch(function(error) {
+      // An error happened.
+    });
+  }
+
+
+  render(){
+    return(
+      <div>
+        <h1>Home</h1>
+        <button onClick={this._logout}>Logout</button>
+      </div>
+    )
+  }
 }
 
 export default Home
