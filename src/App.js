79b83dfdcba260175091237dcb56a068f56e8f8a
Ibrahim <baim008@gmail.com>
2018-09-14 15:09:11 +0800

    added auth state

--- src/App.js
+++ src/App.js
@@ -16,21 +16,58 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import PrivateRoute from "./PrivateRoute";
+import app from "./base";
+
 import Home from "./Home";
-import Login from "./Login/index.js";
+import LogIn from "./Login/index.js";
 import SignUp from "./SignUp/index.js";
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-      </div>
-    </Router>
-  );
-};
+class App extends React.Component {
+  state = { loading: true, authenticated: false, user: null };
+
+  componentWillMount() {
+  app.auth().onAuthStateChanged(user => {
+    if (user) {
+      this.setState({
+        authenticated: true,
+        currentUser: user,
+        loading: false
+      });
+    } else {
+      this.setState({
+        authenticated: false,
+        currentUser: null,
+        loading: false
+      });
+    }
+  });
+}
+
+
+  render(){
+    const { authenticated, loading } = this.state;
+
+    if (loading) {
+      return <p>Loading..</p>;
+    }
+
+    return (
+      <Router>
+        <div>
+          <PrivateRoute
+            exact
+            path="/"
+            component={Home}
+            authenticated={authenticated}
+          />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    )
+  }
+}
 
 export default App;
 
